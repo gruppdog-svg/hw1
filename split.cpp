31efba72f1e4bd193a84837392035ec3caf6a77a
Walker Grupp <wgrupp@usc.edu>
2026-09-09 01:45:56 +0000

    Initial commit for hw1

--- split.cpp
+++ split.cpp
@@ -18,6 +18,25 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if(in == nullptr){ //base case, when this runs then it will return to main, allowing the previously pause SPLIT to finish
+    return;
+  }
+
+  Node* head = in; //new Node* head for each split
+  in = in->next; //in is being used to make new heads over and over until it eventually points to null
+
+  split(in, odds, evens); //pauses the current split to begin the next one
+
+  if(head->value % 2 == 0){
+    head->next = evens;
+    evens = head;
+  }
+  else{
+    head->next = odds;
+    odds = head;
+  }
+  
 }
 
 /* If you needed a helper function, write it here */
+
