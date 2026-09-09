282787bea9cbacef489871e760e9eb5e32367ea4
Walker Grupp <wgrupp@usc.edu>
2026-09-09 03:17:34 +0000

    all done with part 2

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,86 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+void ULListStr::push_back(const std::string& val){
+  if(tail_ == NULL){
+    head_ = tail_ = new Item();
+  }
+  else if(tail_->last == ARRSIZE){
+    Item* n = new Item();
+    n->prev = tail_;
+    tail_->next = n;
+    tail_ = n;
+  }
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+void ULListStr::push_front(const std::string& val){
+  if(head_ == NULL){
+    head_ = tail_ = new Item();
+    head_->first = head_->last = ARRSIZE;
+  } else if(head_->first == 0){
+    Item* n = new Item();
+    n->first = n->last = ARRSIZE;
+    n->next = head_;
+    head_->prev = n;
+    head_ = n;
+  }
+  head_->first--;
+  head_->val[head_->first] = val;
+  size_++;
+}
+
+void ULListStr::pop_back(){
+  tail_->last--;
+  size_--;
+  if(tail_->first == tail_->last){
+    Item* doomed = tail_;
+    tail_ = tail_->prev;
+    if (tail_ != NULL){
+      tail_->next = NULL;
+    } else{
+      head_ = NULL;
+    }
+    delete doomed;
+  }
+}
+
+void ULListStr::pop_front(){
+  head_->first++;
+  size_--;
+  if(head_->last == head_->first){
+    Item* doomed = head_;
+    head_ = head_->next;
+    if(head_ != NULL){
+      head_->prev = NULL;
+    }else{
+      tail_ = NULL;
+    }
+    delete doomed;
+  }
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc)const{
+  Item* cur = head_;
+  size_t seen = 0;
+  while(cur != NULL){
+    size_t count = cur->last - cur->first;
+    if(loc < seen + count){
+      return &cur->val[cur->first + (loc - seen)];
+    }
+    seen += count;
+    cur = cur->next;
+  }
+  return NULL;
+}
+
+std::string const & ULListStr::front()const{
+  return head_->val[head_->first];
+}
+
+std::string const & ULListStr::back()const{
+  return tail_->val[tail_->last-1];
+}
